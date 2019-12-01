bd7adcb380e3deab83458712b9e8514dc6a34575
bmsteven <bmsteve96@gmail.com>
2019-12-01 21:45:55 +0300

    completed the ui of the landing page for desktop

--- bm-dev/src/pages/Home.js
+++ bm-dev/src/pages/Home.js
@@ -5,11 +5,17 @@ import "../styles/style.css"
 
 //components
 import Header from "../components/Header"
+import Main from "../components/Main"
+import Contact from "../components/Contact"
+import Footer from "../components/Footer"
 
 export default function Home() {
     return (
-        <div>
-           <Header/>
+        <div id="home" className="home">
+            <Header/>
+            <Main/>
+            <Contact/>
+            <Footer/>
         </div>
     )
 }
